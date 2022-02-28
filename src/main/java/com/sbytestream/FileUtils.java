430da12bfb8586bdab9a0288be7a12932a7b6718
siddharthbarman <sbytestream@outlook.com>
2022-02-28 20:19:04 +0530

    Changed the way sqlfile is being read

--- src/main/java/com/sbytestream/FileUtils.java
+++ src/main/java/com/sbytestream/FileUtils.java
@@ -1,18 +1,12 @@
 package com.sbytestream;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class FileUtils {
     public static String readTextFileContent(String filePath) throws IOException {
-        FileReader reader = new FileReader(filePath);
-        BufferedReader bufferedReader = new BufferedReader(reader);
-        StringBuilder sb = new StringBuilder();
-        String line;
-        while ((line = bufferedReader.readLine()) != null) {
-            sb.append(line);
-        }
-        return sb.toString();
+        String result = new String(Files.readAllBytes(Paths.get(filePath)));
+        return result;
     }
 }
